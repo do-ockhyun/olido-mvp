d1af046aa64d9151cb6777f1d5ca640076ca7217
do-ockhyun <do-ockhyun@naver.com>
2022-10-30 22:20:11 +0900

    create project

---
+++ src/main/java/com/misolab/olido/OlidoApplication.java
@@ -0,0 +1,12 @@
+package com.misolab.olido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlidoApplication.class, args);
+	}
+}
