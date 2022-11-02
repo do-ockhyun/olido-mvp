8c1fcb3140ca4d89332d24ea47fd6ce294fb8933
do-ockhyun <do-ockhyun@naver.com>
2022-11-02 21:58:11 +0900

    .

--- src/main/resources/static/js/study/exam.js
+++ src/main/resources/static/js/study/exam.js
@@ -2,17 +2,32 @@ new Vue({
   el: "#app",
   data() {
     return {
-      exams: [{
-        id: 0,
-        title: "1번",
-        type: 'MC',
-        c: ['1','2','3','4','5'],
-      },{
-        id: 1,
-        title: "2번",
-        type: 'SA',
-      }]
+      exams: [
+        {
+          id: 0,
+          title: "1번",
+          type: 'MC',   // multiple choice
+          choice: [1,2,3,4,5],
+          answer: null,
+        },
+        {
+          id: 1,
+          title: "2번",
+          type: 'SA',   //  single answer
+          answer: null,
+        },
+        {
+          id: 2,
+          title: "3번",
+          type: 'MF',   //  math form
+          answer: null,
+        },
+      ],
     };
   },
-  methods: {},
+  methods: {
+    clearAnswer(ex) {
+      ex.answer = null;
+    },
+  },
 });
