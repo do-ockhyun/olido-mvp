de583bcb73530a044406876542c1abc265e53b29
do-ockhyun <do-ockhyun@naver.com>
2022-11-08 22:54:40 +0900

    katex, popup

--- src/main/resources/static/js/study/exam.js
+++ src/main/resources/static/js/study/exam.js
@@ -1,112 +1,74 @@
-function cleanLatex(latexExport) {
-  if (latexExport.includes("\\\\")) {
-    const steps = "\\begin{align*}" + latexExport + "\\end{align*}";
-    return steps
-      .replace("\\begin{aligned}", "")
-      .replace("\\end{aligned}", "")
-      .replace(new RegExp("(align.{1})", "g"), "aligned");
-  }
-  return latexExport.replace(new RegExp("(align.{1})", "g"), "aligned");
-}
-
-new Vue({
+const vm = new Vue({
   el: "#app",
-  mounted() {    
-    console.log('refs', this.$refs);
-
-    this.editor = iink.register(this.$refs.editor, {
-      recognitionParams: {
-        type: "MATH",
-        server: {
-          applicationKey: "515131ab-35fa-411c-bb4d-3917e00faf60",
-          hmacKey: "54b2ca8a-6752-469d-87dd-553bb450e9ad",
-        },
-      },
-    });
-
-    setTimeout(() => {
-      this.selected.visible = false
-    }, 1500)
-
-    this.$refs.editor.addEventListener('exported', (evt) => {
-      const exports = evt.detail.exports;
-      if (exports && exports['application/x-latex']) {
-        this.selected.ex.answer = cleanLatex(exports['application/x-latex']);
-        katex.render(this.selected.ex.answer,  this.selected.result);
-      }
-    });
-
-  },
-  destroyed() {
-    this.editor.close();
+  mounted() {
   },
 
   data() {
     return {
-      selected: {
-        title: "수식입력",
-        visible: true,
-        ex: null
-      },
+      selected: null,
       exams: [
         {
           id: 0,
           title: "1번",
-          type: 'MC',   // multiple choice
-          choice: [1,2,3,4,5],
+          type: "MC", // multiple choice
+          choice: [1, 2, 3, 4, 5],
           answer: null,
         },
         {
           id: 1,
           title: "2번",
-          type: 'SC',   //  single choice
+          type: "SC", //  single choice
           answer: null,
         },
         {
           id: 2,
           title: "3번",
-          type: 'SA',   //  single answer
+          type: "SA", //  single answer
           answer: null,
         },
         {
           id: 3,
           title: "4번",
-          type: 'MF',   //  math form
+          type: "MF", //  math form
           answer: null,
-          result: null
+          result: null,
         },
       ],
     };
   },
   methods: {
-    convert() {
-      this.editor.convert();
-      this.closeMahtForm();
-    },
-    clear() {
-      this.editor.clear();
-    },
-
-    showMathForm(ex){
-      this.selected.ex = ex
-      this.selected.visible = true
-      this.selected.result = this.$refs.result[0]
-    },
-    closeMahtForm(){
-      this.selected.ex = null
-      this.selected.visible = false
-    },
     clearAnswer(ex) {
-      ex.answer = null;
-      if (ex.type === "MF") {
-        this.selected.result.innerHTML = "수식입력 입력하시려면 터치하세요";
+      ex.answer = null
+      if (ex.result) {
+        ex.result = null
       }
+    },
+    showMathForm(ex){
+      this.selected = ex;
+      openPopup()
+    },
+    closeMathForm(answer) {
+      this.selected.answer = answer;
+      this.selected.result = katex.renderToString(answer);
+      this.selected = null;
     },
     goHome() {
-      const ok = confirm("제출하시겠습니까?") 
+      const ok = confirm("제출하시겠습니까?");
       if (ok) {
-        location.href = "/study"  
+        location.href = "/study";
       }
-    }
+    },
   },
 });
+
+function openPopup() {
+  window.open('/mathform.html')
+}
+
+function returnPopup(params) {
+  console.log('returnPopup', params);
+  setTimeout(() => {
+    vm.closeMathForm(params);
+  }, 100);  
+}
+
