15cabc1807ba4c5ab12b97ea7c86d1d2d8f92e22
do-ockhyun <do-ockhyun@naver.com>
2022-11-09 23:43:00 +0900

    login

--- README.md
+++ README.md
@@ -2,35 +2,12 @@
 
 issue - step을 진행하자
 
-1. 학습지 제작 > 제출 > 통계
-  - path를 만들자
-    * /
-    * /login
-    * /study
-      - /home
-      - /exam/{id}
-      - /result/{id}
-    * /admin
-      - /quiz
-      - /exam
-      - /result
-  - UI를 만들자
-    * /
-    * /login
-    * /study
-      - /home
-      - /exam/{id}
-      - /result/{id}
-
-    * /admin
-      - /quiz
-      - /exam
-      - /result
-  - API를 만들자
-    * 
-
-
-
+1. 학생앱 만들
+  - 홈 : /
+  - 로그인 : /login
+  - 목록 : /study
+  - 시험 : /study/exam/{id}
+  - 결과 : /study/result/{id}
 
 # TODO
 - prod, dev 환경
--- src/main/java/com/misolab/olido/web/AppController.java
+++ src/main/java/com/misolab/olido/web/AppController.java
@@ -1,16 +1,57 @@
 package com.misolab.olido.web;
 
+import java.util.ArrayList;
+
+import javax.annotation.PostConstruct;
+
+import org.omg.CosNaming.NamingContextPackage.NotFound;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 public class AppController {
 
-    @GetMapping("/login")
+	@Data
+	@AllArgsConstructor
+	static class User {
+		String userId;
+		String name;
+	}
+
+	ArrayList<User> users = new ArrayList<>();
+
+	@PostConstruct
+	public void onCreate() {
+		users.add(new User("user1", "도세현"));
+		users.add(new User("user2", "도옥현"));
+		users.add(new User("user3", "올리도"));
+	}
+
+	@GetMapping("/login")
 	public String login(Model model) {
 		model.addAttribute("name", "Login page");
 		return "login";
+	}
+
+	@ResponseBody
+	@PostMapping("/login")
+	public User postLogin(String userId) throws Exception {
+		log.info("postLogin {}", userId);	
+
+		User user = users.stream()
+		.filter(u -> u.userId.equalsIgnoreCase(userId))
+		.findFirst()
+		.orElseThrow(() -> new Exception(userId + " is Not Found"));
+
+		return user;
 	}
 
 }
